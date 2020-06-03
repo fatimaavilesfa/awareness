04855329ff848c1565b4b0780e195957860e1f23
fatima_aviles@ksquare.university <fatimaaviles1612@gmail.com>
2020-06-02 19:15:53 -0500

    added question to form and added spans

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,33 @@ const form = document.querySelector( "form" )
  
 //function to get value
 const getValue = (options) =>  {
-  let result = [];
-  for (let i = 0, length = options.length; i < length; i++) {
+  let result = [],
+      length = options.length;
+  for (let i = 0 ; i < length; i++) {
     if (options[i].checked) {
       result.push(options[i].value);
     }
   }
   return result;
 }
+//to display  relatives options
+function hasFam(element) {
+  if(element.id === 'no') {
+    document.getElementById('yesFam').innerHTML = "";
+  }
+  if(element.id === 'yes') {
+    document.getElementById('yesFam').innerHTML = `
+    <p>Slect the ones that apply</p>
+    <input type="checkbox" id="mother" value="mother" name="famMemb">
+                <label for="mother">Mother</label><br>
+                <input type="checkbox" id="father" value="father" name="famMemb">
+                <label for="father">Father</label><br> 
+                <input type="checkbox" id="siblings" value="siblings" name="famMemb">
+                <label for="siblings">Siblings</label><br>
+    `;
+  }
+}
+
   // ...to take over the submit event
   form.addEventListener( 'submit', function ( event ) {
     //method cancels the event if it is cancelable
@@ -18,10 +37,10 @@ const getValue = (options) =>  {
     let gender = getValue(document.getElementsByName('gender')),
         age =  getValue(document.getElementsByName('age')),
         active = getValue(document.getElementsByName('activity')), 
-        relatives = getValue(document.getElementsByName('family')),
-        pressure = getValue(document.getElementsByName('pressure')),
+        relatives = getValue(document.getElementsByName('famMemb')),
+        symptoms = getValue(document.getElementsByName('symptoms')),
         popup = document.getElementById("myPopup"),
-        sugar = document.getElementById('sugar').value,
+        pressure = document.getElementById('pressure').value,
         score = 0,
         message,
         bmi;
@@ -33,47 +52,63 @@ const getValue = (options) =>  {
     let weight = Number(document.getElementById('weight').value);
     bmi =  weight / (height * height);
     
+    //add point to score
     if(gender[0] === 'male') {
       score++;
     }
+
     if(age[0] === '59') {
       score++;
     }
     else if(age[0] === '60') {
       score += 2;
     }
+
     if(active[0] === 'no') {
       score++;
     }
+
     if(bmi < 29.9 && bmi > 24.9) {
       score++;
     }
     else if(bmi > 30) {
       score += 2;
     }
+
     if(relatives.length === 1) {
       score++;
     }
     else if(relatives.length > 1) {
       score += 2;
     }
-    if(sugar !== 'no') {
-      score++;
+
+    if(symptoms.length > 2 && 4 < symptoms.length) {
+      score ++;
     }
-    if(pressure === 'yes') {
+    else if(symptoms.length > 4) {
+      score += 2;
+    }
+
+
+    if(pressure === 'yes' || pressure == 140 ) {
       score++;
+    } 
+    else if (pressure ==='hyper') {
+      score += 2;
     }
    
-    if(score < 3) {
+
+    //mesure the risk
+    if(score < 4) {
       message = "Low risk!"
     }
-    else if(score < 7) {
+    else if(score < 9) {
       message = "Medium risk!"
     } else {
       message = "High risk, please visit a doctor!"
     }
    
-    console.log(gender, age, active, relatives, bmi, sugar)
+    console.log(gender, age, active, relatives, bmi)
 
  //to display the result of test
  popup.textContent = message;
@@ -86,5 +121,7 @@ const getValue = (options) =>  {
     document.getElementById('formid').reset()
   }, 3000);
 });
+
+
 
 
--- public/js/appindex.js
+++ public/js/appindex.js
@@ -1,6 +1,46 @@
 
 function responsive(item) {
     const num = item.id;
-    document.getElementById(`text${num}`).style.visibility = 'visible'
-    //document.getElementById(num).style.padding = '11px';
+    let x = document.getElementById('t'+num).innerHTML
+    console.log(typeof x, x == '')
+    if(x == '') {
+        if(num == '1') {
+            document.getElementById('t'+num).innerHTML = `
+                <p>
+                    If you have type 1 diabetes, your body does not make insulin. Your immune system attacks and destroys the cells in your 
+                    pancreas that make insulin. Type 1 diabetes is usually diagnosed in children and young adults, although it can appear at 
+                    any age. People with type 1 diabetes need to take insulin every day to stay alive.
+                </p>`
+        }
+        if(num == 2) {
+            document.getElementById('t'+num).innerHTML = `
+                <p>
+                    If you have type 2 diabetes, your body does not make or use insulin well. You can develop type 2 diabetes at any age, even
+                    during childhood. However, this type of diabetes occurs most often in middle-aged and older people. Type 2 is the most common 
+                    type of diabetes.
+                </p>
+            `
+        }
+        if(num == 3) {
+            document.getElementById('t'+num).innerHTML = `
+                <p>
+                Gestational diabetes develops in some women when they are pregnant. Most of the time, this type of diabetes goes away after the
+                baby is born. However, if you’ve had gestational diabetes, you have a greater chance of developing type 2 diabetes later in life.
+                Sometimes diabetes diagnosed during pregnancy is actually type 2 diabetes.
+                </p>
+            `
+        }
+        if(num == 4) {
+            document.getElementById('t'+num).innerHTML = `
+                <p>
+                Occurs when your blood sugar is higher than normal, but it’s not high enough for a diagnosis of type 2 diabetes.
+                If there is no action taken it can eventually become type 2 diabetes.
+                </p>
+            `
+        }
+        
+    } 
+    else {
+        document.getElementById('t'+num).innerHTML = ''
+    }
 }
